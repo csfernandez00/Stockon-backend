fbb0c5b0cbf567c14e867a3e28f586f8e4fdb776
csfernandez00 <csfernandez00@gmail.com>
2022-09-05 12:24:28 -0300

    Backend linked

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,12 +1,4 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Patch,
-  Post,
-} from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { Product } from './product.model';
 import { Products } from './products.entity';
 import { ProductsService } from './products.service';
